d28e74749e1c4f56c17fbb8c123e901e63cddcbc
javier-la200426 <javier.la200426@gmail.com>
2025-02-22 16:08:52 -0500

    Feb22

---
+++ JumboJob/app/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Questions from "./Components/questions"; // Import Questions.tsx
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Questions />  {/* Load your Questions component */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1a1a1a",
+  },
+});
